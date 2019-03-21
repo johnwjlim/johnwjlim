cc11bc5e0cc442690faf9e5c9847ca2248b2ae8b
John Lim <wjlim@uw.edu>
2019-03-21 16:49:05 -0700

    content added

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,6 +6,7 @@ exports.components = {
   "component---cache-dev-404-page-js": preferDefault(require("/Users/wjlim/code/johnwjlim/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/wjlim/code/johnwjlim/src/pages/404.js")),
   "component---src-pages-about-jsx": preferDefault(require("/Users/wjlim/code/johnwjlim/src/pages/about.jsx")),
+  "component---src-pages-google-jsx": preferDefault(require("/Users/wjlim/code/johnwjlim/src/pages/google.jsx")),
   "component---src-pages-index-js": preferDefault(require("/Users/wjlim/code/johnwjlim/src/pages/index.js")),
   "component---src-pages-nimbus-jsx": preferDefault(require("/Users/wjlim/code/johnwjlim/src/pages/nimbus.jsx")),
   "component---src-pages-ohana-jsx": preferDefault(require("/Users/wjlim/code/johnwjlim/src/pages/ohana.jsx")),
